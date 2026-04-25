8f6dc8d972a94c2a603dd0c107d22ba23a69e0a8
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-04-25 18:07:36 -0300

    punto 3: prueba de calculos en el main (antes de implememntar en funciones fuera del main) y correcciones para preciosUnitarios de Productos (implementando arreglos)

--- tp3_3.c
+++ tp3_3.c
@@ -3,6 +3,9 @@
 #include <time.h>
 #include <string.h>
 
+// CONSTANTES
+#define N 5
+
 // DISTRIBUIDORA
 
 // ESTRUCTURAS
@@ -22,12 +25,30 @@ typedef struct Cliente{
                                     // "cantidadProductosAPedir"
 } Cliente;
 
+// FUNCIONES PARA EL SISTEMA
+//float calcularCostoTotalProducto(Producto * producto);
+
 
 int main()
 {
+    // semilla para el rand()
     srand(time(NULL));
-    char *TiposProductos[] = {"Galletas", "Snacks", "Cigariilos", "Caramelos", "Bebidas"};
-    int cantidad_tipo_prod = 5;
+    // arreglo de los tipos de productos
+    char *TiposProductos[N] = {"Galletas", "Snacks", "Cigariilos", "Caramelos", "Bebidas"};
+    float preciosTiposProductos[N]; // defino un arreglo que contenga los precios unitarios de cada tipo de producto
+    int precio_min=10, precio_max=100; // defino un rango de valores para el precio
+    float precio_unit_producto, parteEntera, parteDecimal; // parte entera y decimal para liuego ser sumadas
+    
+    // calculo el precio para cada item del arreglo 'preciosTiposProductos[]'
+    for (int tipoProducto_i = 0; tipoProducto_i < N; tipoProducto_i++)
+    {
+        parteEntera = (float) (precio_min + rand() % (precio_max-precio_min+1));
+        parteDecimal = (float) (rand() % (100+1)) / (float) 100;
+        preciosTiposProductos[tipoProducto_i] = parteEntera + parteDecimal;
+    }
+    //
+    
+    int cantidad_tipo_prod = N;
     // INTERFAZ DE USUARIO: solicitar la cantidad de clientes //
     int cantidadClientes;
     printf("\n----- INTERFAZ DE USUARIO -----\n");
@@ -37,13 +58,13 @@ int main()
     // reservo memoria para un arreglo dinamico de CLIENTES
     Cliente * clientes = (Cliente *) malloc(sizeof(Cliente) * cantidadClientes);
 
-    printf("\n-->CARGA DE CLIENTES<--\n");
+    printf("\n-->CARGA DE CLIENTE(S)<--\n");
     int i, opcion;
-    int precio_min=10, precio_max=100;
-    float precio_unit_producto, parteEntera, parteDecimal;
+    float costoTotalProducto_j, costoTotalAPagar; // guardo el costo total calculado para cada tipo de producto elegido por cliente
     char Buff[50];
     for(i = 0; i < cantidadClientes; i++)
     {
+        costoTotalAPagar = 0;
         // ID_CLIENTE
         clientes[i].clienteID = i;
         // NOMBRE_CLIENTE
@@ -56,7 +77,7 @@ int main()
         clientes[i].cantidadProductosAPedir = 1 + rand() % 5;
         // CARGA_DE_PRODUCTOS
         clientes[i].productos = (Producto *) malloc(sizeof(Producto) * clientes[i].cantidadProductosAPedir);
-        printf("\n-->CARGA DE PRODUCTOS<-- (para cliente[%d]-> %s)", i+1, clientes[i].nombreCliente);
+        printf("\n-->CARGA DE PRODUCTO(S)<-- (para cliente[%d]-> %s)", i+1, clientes[i].nombreCliente);
         printf("\nCantidad de Productos para elegir: %d", clientes[i].cantidadProductosAPedir);
         for (int j = 0; j < clientes[i].cantidadProductosAPedir; j++)
         {
@@ -75,11 +96,27 @@ int main()
             scanf("%d",&opcion);      
             (clientes + i)->productos[j].TipoProducto = TiposProductos[opcion-1];
             // PRECIO_UNITARIO_producto
-            parteEntera = (float) (precio_min + rand() % (precio_max-precio_min+1));
-            parteDecimal = (float) (rand() % (100+1)) / (float) 100;
-            (clientes + i)->productos[j].precioUnitario = parteEntera + parteDecimal;
+            (clientes + i)->productos[j].precioUnitario = preciosTiposProductos[opcion-1];
+            //
+            // Calculo el costo Total del producto[j] (para un producto específico)
+            costoTotalProducto_j = (clientes + i)->productos[j].precioUnitario * (clientes + i)->productos[j].cantidad;
+            //
+            printf("\n******\nEl precio unitario del producto '%s' es $%f \ny la cantidad llevada es %d .\nEl costo total de este producto es : $%f\n******\n",
+                (clientes + i)->productos[j].TipoProducto,
+                (clientes + i)->productos[j].precioUnitario, 
+                (clientes + i)->productos[j].cantidad, 
+                costoTotalProducto_j );
+            //
+            // costo TOTAL por CLIENTE
+            costoTotalAPagar = costoTotalAPagar + costoTotalProducto_j;
+            //
         }
-
+        // muestro el total a pagar por CLIENTE
+        printf("\n\nEl monto final a pagar para el cliente[%d] -> %s, es: %f\n",
+            i+1, 
+            clientes[i].nombreCliente, 
+            costoTotalAPagar );
+        
     }
     
 
@@ -95,3 +132,14 @@ int main()
     printf("\n----- FIN DEL PROGRAMA -----\n");
     return 0;
 }
+
+/*
+    Esta función debe recibir como parámetro el producto y 
+    devolver el resultado de calcular la Cantidad por el PrecioUnitario.
+*/
+// float calcularCostoTotalProducto(Producto * producto);
+// {
+//     int i = 0;
+//     int cantidad = producto->cantidad;
+//     float precio = producto->precioUnitario;
+// }
