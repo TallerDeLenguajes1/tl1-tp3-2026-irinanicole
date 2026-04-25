1916d89a91ec46b62a943458a6fe23a7d7a2a833
Nicky Bourdette (Hp) <inbourdette.est@gmail.com>
2026-04-25 20:11:49 -0300

    Punto 3: remil CHECK

--- tp3_3.c
+++ tp3_3.c
@@ -27,15 +27,21 @@ typedef struct Cliente{
 
 // FUNCIONES PARA EL SISTEMA
 float calcularCostoTotalProducto(Producto producto_j); // devuelve el costo total del producto (cantidad * precioUnitario)
+void mostrarVentas(Cliente * clientes, int cant_clientes);
 
 
 int main()
 {
     // semilla para el rand()
     srand(time(NULL));
+
     // arreglo de los tipos de productos
     char *TiposProductos[N] = {"Galletas", "Snacks", "Cigariilos", "Caramelos", "Bebidas"};
+    int cantidad_tipo_prod = N;
+    // arreglo para los precios de los tipos de productos (valores flotantes aleatorios entre 10 y 100)
     float preciosTiposProductos[N]; // defino un arreglo que contenga los precios unitarios de cada tipo de producto
+    
+    // defino parámetros para formar los numeros aleatorios flotantes para el arreglo 'preciosTiposProductos[]'
     int precio_min=10, precio_max=100; // defino un rango de valores para el precio
     float precio_unit_producto, parteEntera, parteDecimal; // parte entera y decimal para liuego ser sumadas
     
@@ -47,8 +53,7 @@ int main()
         preciosTiposProductos[tipoProducto_i] = parteEntera + parteDecimal;
     }
     //
-    
-    int cantidad_tipo_prod = N;
+
     // INTERFAZ DE USUARIO: solicitar la cantidad de clientes //
     int cantidadClientes;
     printf("\n----- INTERFAZ DE USUARIO -----\n");
@@ -59,35 +64,38 @@ int main()
     Cliente * clientes = (Cliente *) malloc(sizeof(Cliente) * cantidadClientes);
 
     printf("\n-->CARGA DE CLIENTE(S)<--\n");
-    int i, opcion;
-    float costoTotalProducto_j, costoTotalAPagar; // guardo el costo total calculado para cada tipo de producto elegido por cliente
+    int opcion;
     char Buff[50];
-    for(i = 0; i < cantidadClientes; i++)
+    for(int i = 0; i < cantidadClientes; i++)
     {
-        costoTotalAPagar = 0;
         // ID_CLIENTE
         clientes[i].clienteID = i;
+        
         // NOMBRE_CLIENTE
         getchar();
         printf("\nNOMBRE cliente %d: ", i+1);
         gets(Buff);
+        // reservo memoria para el nombre del cliente
         clientes[i].nombreCliente = (char *) malloc(sizeof(char) * strlen(Buff));
         strcpy(clientes[i].nombreCliente, Buff);
+        
         // CANTIDAD_PRODUCTOS_A_PEDIR-> ALEATORIOS (entre 1 y 5)
         clientes[i].cantidadProductosAPedir = 1 + rand() % 5;
+        
         // CARGA_DE_PRODUCTOS
+        // reservo memoria para cada producto 'cantidadProductosAPedir' veces...
         clientes[i].productos = (Producto *) malloc(sizeof(Producto) * clientes[i].cantidadProductosAPedir);
-        printf("\n-->CARGA DE PRODUCTO(S)<-- (para cliente[%d]-> %s)", i+1, clientes[i].nombreCliente);
+        printf("\n-->CARGA DE PRODUCTO(S)<-- (cliente[%d] -> %s)", i+1, clientes[i].nombreCliente);
         printf("\nCantidad de Productos para elegir: %d", clientes[i].cantidadProductosAPedir);
+        // comienzo a llenar los datos en cada producto para 'cliente[i]':
         for (int j = 0; j < clientes[i].cantidadProductosAPedir; j++)
         {
-            printf("\n**Producto[%d]**",j+1);
             // ID_producto
             (clientes + i)->productos[j].productoID = j;
             // CANTIDAD del producto
             (clientes + i)->productos[j].cantidad = 1 + rand() % 10;
             // TIPO_producto
-            printf("\n>>Eliga OPCION para resgistrar el TIPO DE PRODUCTO<<");
+            printf("\n**Producto[%d]**\n>>Eliga OPCION para resgistrar el TIPO DE PRODUCTO<<",j+1);
             for (int k = 0; k < cantidad_tipo_prod; k++)
             {
                 printf("\nOp:%d -> %s",k+1,TiposProductos[k]);
@@ -98,38 +106,25 @@ int main()
             // PRECIO_UNITARIO_producto
             (clientes + i)->productos[j].precioUnitario = preciosTiposProductos[opcion-1];
             //
-            // Calculo el costo Total del producto[j] (para un producto específico)
-            costoTotalProducto_j = calcularCostoTotalProducto((clientes+i)->productos[j]); //implemento funcion para el calculo
-            //
-            printf("\n******\nEl precio unitario del producto '%s' es $%f \ny la cantidad llevada es %d .\nEl costo total de este producto es : $%f\n******\n",
-                (clientes + i)->productos[j].TipoProducto,
-                (clientes + i)->productos[j].precioUnitario, 
-                (clientes + i)->productos[j].cantidad, 
-                costoTotalProducto_j );
-            //
-            // costo TOTAL por CLIENTE
-            costoTotalAPagar = costoTotalAPagar + costoTotalProducto_j;
-            //
         }
-        // muestro el total a pagar por CLIENTE
-        printf("\n\nEl monto final a pagar para el cliente[%d] -> %s, es: %f\n",
-            i+1, 
-            clientes[i].nombreCliente, 
-            costoTotalAPagar );
-        
     }
     
+    // llamo a la funcion mostrarVentas() e imprimo todo por pantalla
+    mostrarVentas(clientes, cantidadClientes);
+    //
 
-    // LIBERO MEMORIA: por cada PRODUCTOs creados y el NOMBRE_CLIENTE
-    for (i = 0; i < cantidadClientes; i++)
+    // LIBERO MEMORIA: por todos los PRODUCTOS CREADOS para un cliente y el NOMBRE_CLIENTE
+    for (int index = 0; index < cantidadClientes; index++)
     {
-        free(clientes[i].productos);
-        free(clientes[i].nombreCliente);
+        free(clientes[index].productos);
+        free(clientes[index].nombreCliente);
     }
     // LIBERO MEMORIA: por el arreglo de todos los clientes
     free(clientes);
     //
+
     printf("\n----- FIN DEL PROGRAMA -----\n");
+    getchar();
     return 0;
 }
 
@@ -140,4 +135,38 @@ int main()
 float calcularCostoTotalProducto(Producto producto_j)
 {
     return (producto_j.precioUnitario * producto_j.cantidad);
+}
+
+void mostrarVentas(Cliente * clientes, int cant_clientes)
+{
+    float costoTotalProducto_j, costoTotalAPagar;
+    printf("\n\n---> MUESTRA DE DATOS: CLIENTES <---\n\n");
+    for (int i = 0; i < cant_clientes; i++)
+    {
+        costoTotalAPagar = 0;
+        printf("\n------------------------------");
+        //imprimo DATOS del cliente.
+        printf("\n>>[CLIENTE: %d]<<\nID_cliente: %d\nNombre: %s\nProductos Solicitados: %d\n",
+            i+1, 
+            clientes[i].clienteID, 
+            clientes[i].nombreCliente, 
+            clientes[i].cantidadProductosAPedir);
+        for (int j = 0; j < clientes[i].cantidadProductosAPedir; j++)
+        {
+            printf("\n**[PRODUCTO: %d]**\nID_producto: %d\nTipo: %s\nPrecio(xUnidad): $%f\nCantidad: %d",
+                j+1, 
+                clientes[i].productos[j].productoID, 
+                clientes[i].productos[j].TipoProducto, 
+                clientes[i].productos[j].precioUnitario, 
+                clientes[i].productos[j].cantidad);
+            // Calculo el costo Total del producto[j] (para un producto específico)
+            costoTotalProducto_j = calcularCostoTotalProducto((clientes+i)->productos[j]); //implemento funcion para el calculo
+            printf("\n---> El costo total de este prodcuto es: $%f\n", costoTotalProducto_j);
+            costoTotalAPagar += costoTotalProducto_j; // voy sumando todos los 'costoTotalProducto_j' de cada producto;
+        }
+        printf("\n------------------------------");
+        printf("\nEl monto total a pagar por este cliente es: $%f", costoTotalAPagar);
+        printf("\n------------------------------\n");
+    }
+    
 }
